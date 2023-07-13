62a040c0c4700a68c7d60f9f989a7c110bb2d095
Vitaliy1803 <35754722+Vitaliy1803@users.noreply.github.com>
2023-07-13 11:43:36 +0300

    feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+// 1.Відстежуй на формі подію input, і щоразу записуй у локальне сховище об'єкт з полями email і message, у яких зберігай поточні значення полів форми. Нехай ключем для сховища буде рядок "feedback-form-state".
+// 2. Під час завантаження сторінки перевіряй стан сховища, і якщо там є збережені дані, заповнюй ними поля форми. В іншому випадку поля повинні бути порожніми.
+// 3. Під час сабміту форми очищуй сховище і поля форми, а також виводь у консоль об'єкт з полями email, message та їхніми поточними значеннями.
+// 4. Зроби так, щоб сховище оновлювалось не частіше, ніж раз на 500 мілісекунд. Для цього додай до проекту і використовуй бібліотеку lodash.throttle.
 
+import throttle from "lodash.throttle";
+
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', throttle(onFormData, 500));
+form.addEventListener('submit', onSubmitForm);
+
+const dataForm = {};
+
+function onFormData(event) {
+    dataForm[event.target.name] = event.target.value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(dataForm));
+};
+
+function onSubmitForm(event) {
+    console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+    event.preventDefault();
+    event.currentTarget.reset();
+    localStorage.removeItem('feedback-form-state');
+};
+
+function localDataForm() {
+   const savedDataForm = JSON.parse(localStorage.getItem('feedback-form-state'));
+   const email = document.querySelector('.feedback-form input');
+   const message = document.querySelector('.feedback-form textarea');
+
+
+   if (savedDataForm) {
+    email.value = savedDataForm.email;
+    message.value = savedDataForm.message;
+   }
+};
+
+localDataForm();
